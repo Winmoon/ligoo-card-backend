0389364c232b0835f12003ed2a4efac6bbb35b08
Diego M. Rodrigues <diegomr86@gmail.com>
2013-10-16 15:27:52 -0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LigooCard::Application.config.secret_key_base = '0c12957180d7d134eab9a704f852280fdb15f97ffe260ec38abaa9386558e0ab5a8e482b39b21cb1ca4084d9d6908cff3dab380766e6b39247262352e091c4c6'
