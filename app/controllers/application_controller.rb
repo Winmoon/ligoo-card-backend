190e6e0cb1d6989aad326d8abae115d1abac0cb4
Winmoon 3 <wendell.palazzo@winmoon.com>
2013-11-05 16:02:24 -0200

    Adicionado controle para cross domain no application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_filter :cors_set_access_control_headers
+
+  def cors_set_access_control_headers
+    if request.headers['HTTP_ORIGIN']
+      headers['Access-Control-Allow-Origin'] = request.headers['HTTP_ORIGIN']
+      headers['Access-Control-Expose-Headers'] = 'ETag'
+      headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+      headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match,Auth-User-Token'
+      headers['Access-Control-Max-Age'] = '86400'
+      headers['Access-Control-Allow-Credentials'] = 'true'
+    end
+  end
 
 end
