289c1aea02b83d01748ec6a65af316d7079a15dc
Diego M. Rodrigues <diegomr86@gmail.com>
2013-11-06 11:01:12 -0200

    Configura cors no middleware

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module LigooCard
       DeviseController.respond_to :html, :json
     end
 
-    config.middleware.use Rack::Cors do
+    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
       allow do
         origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
