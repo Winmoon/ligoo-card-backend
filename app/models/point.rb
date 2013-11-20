530137cbc69763f3cab82479e74ad6ed303c254b
Diego M. Rodrigues <diegomr86@gmail.com>
2013-11-20 17:20:48 -0200

    Adiciona a validação para pontos por compartilhamento

--- app/models/point.rb
+++ app/models/point.rb
@@ -4,11 +4,13 @@ class Point < ActiveRecord::Base
   validates :user, :establishment, :point_type, presence: true, length: { :minimum => 0 }
   scope :available, where('coupon_id is null').order('created_at asc')
   scope :used, where('coupon_id is not null').order('created_at asc')
+  scope :qrcode_points, where('point_type = ?', :qrcode)
   scope :share_points, where('point_type = ?', :share)
 
   validate :daily_limit
 
   def daily_limit
-    errors.add(:news, I18n.t("activerecord.attributes.point.errors.daily_limit")) if establishment.daily_point_limit? && user.points.where('establishment_id = ? and created_at > ?', establishment.id, 1.day.ago).exists?
+    errors.add(:point_type, I18n.t("activerecord.attributes.point.errors.daily_limit")) if establishment.daily_point_limit? && user.points.qrcode_points.where('establishment_id = ? and created_at > ?', establishment.id, 1.day.ago).exists?
+    errors.add(:point_type, I18n.t("activerecord.attributes.point.errors.daily_share_limit")) if establishment.daily_point_limit? && user.points.share_points.where('establishment_id = ? and created_at > ?', establishment.id, 1.day.ago).exists?
   end
 end
